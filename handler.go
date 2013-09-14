22e2ceeb3a14e459af0860e9de26a9aed1096fa0
Sarah Adams <sadams.codes@gmail.com>
2013-09-14 02:22:19 -0700

    botling can search for go package definitions

--- handler.go
+++ handler.go
@@ -7,7 +7,10 @@ import (
 
 // Returns the appropriate reply message for a given ping
 func replyMessage(message hipchat.Message) (reply, kind string) {
-	if strings.Contains(message.Body, "logo") {
+	if strings.Contains(message.Body, "gopkg") { 
+		query := strings.Split(message.Body, "gosearch")[1]
+		return goSearch(query), "text"
+	} else if strings.Contains(message.Body, "logo") {
 		return "<img src='" + LOGO_URL + "'/>", "html"
 	} else {
 		return "Hello, " + name(message.From), "text"

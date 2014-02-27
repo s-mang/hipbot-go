bf001b97f09f7a9f28c7d7a07714b549d7300567
Sarah Adams <sadams.codes@gmail.com>
2014-02-27 03:21:09 -0800

    append fullname to room_jid when sending chat message

--- botling.go
+++ botling.go
@@ -131,7 +131,7 @@ func main() {
 					speakInHTML(reply, false)
 				} else {
 					// Plain text messages sent to Hipchat via XMPP
-					botling.Send(xmpp.Chat{Remote: roomJid, Type: "chat", Text: reply})
+					botling.Send(xmpp.Chat{Remote: roomJid + "/" + fullname, Type: "chat", Text: reply})
 				}
 			}
 		} else {
